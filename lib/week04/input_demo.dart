29ecffc6b2502b4e95be85e5c5f4b273477558dd
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-09-05 11:27:37 +0700

    Update the txtfield with decoration and clear text button icon

--- lib/week04/input_demo.dart
+++ lib/week04/input_demo.dart
@@ -8,21 +8,39 @@ class InputDemo extends StatefulWidget {
 }
 
 class _InputDemoState extends State<InputDemo> {
+  // state variables
   String message = '';
+  TextEditingController tcName = TextEditingController();
+
+  //function
+  void updateText() {
+    setState(() {
+      message = tcName.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Input Demo')),
       body: Column(
         children: [
-          TextField(
-            onChanged: (String txt) {
-              // debugPrint(txt);
-              setState(() {
-                message = txt;
-              });
-            },
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your name',
+                suffixIcon: IconButton(
+                  onPressed: tcName.clear,
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+              controller: tcName,
+            ),
           ),
+          ElevatedButton(onPressed: updateText, child: Text('OK')),
+          SizedBox(height: 20),
           Text(message),
         ],
       ),
