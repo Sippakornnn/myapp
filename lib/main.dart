bafae0068ca01da3e743bd01fbac41665163b066
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-09-12 11:39:44 +0700

    Add random integer generator demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week05/design2.dart';
+import 'package:myapp/week05/random_demo.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Design2()
+      home: RandomDemo()
     ),
   );
 }
