76e33d348ced3c9502e89f858b3e466d4f596f6f
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-09-19 11:21:01 +0700

    Add normal Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week06/login_dynamic.dart';
+import 'package:myapp/week06/timer_demo.dart';
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginDynamic()
+      home: TimerDemo()
     ),
   );
 }
---
+++ lib/week06/timer_demo.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimerDemo extends StatefulWidget {
+  const TimerDemo({super.key});
+
+  @override
+  State<TimerDemo> createState() => _TimerDemoState();
+}
+
+class _TimerDemoState extends State<TimerDemo> {
+  String message = "Start";
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      setState(() {
+        message = "Stop";
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Text(message, style: TextStyle(fontSize: 30)),
+        ),
+      ),
+    );
+  }
+}
