f24bf30ce4be329e7073d8de8e745cbb1efe432d
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-09-19 10:37:57 +0700

    Add baking app design

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week06/image_demo.dart';
+import 'package:myapp/week06/baking_app.dart';
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ImageDemo()
+      home: BakingApp()
     ),
   );
 }
