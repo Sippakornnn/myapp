b40d1d4b30f7e18c268f678aeb3f8f988c69f49b
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-09-21 14:38:36 +0700

    Complete a “click as fast as you can” game

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week06/countdown_timer.dart';
+import 'package:myapp/week06/HW/click_game.dart';
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CountdownTimer()
+      home: ClickGame()
     ),
   );
 }
