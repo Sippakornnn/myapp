156b118a935b5c7c928a1ef5c5bce259196c0a5e
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-09-25 15:18:02 +0700

    Add cooking recipe design

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week06/HW/signin_app.dart';
+import 'package:myapp/week06/cooking.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SigninApp()
-    ),
-  );
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Cooking()));
 }
-
-
-
-
