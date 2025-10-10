555d3fcf9b087e8210822fe1976b3b52780e4823
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-10-10 10:53:37 +0700

    radio button demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week07/slider_demo.dart';
+import 'package:myapp/week07/radio_demo.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SliderDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: RadioDemo()));
 }
