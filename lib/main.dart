4cc4177205a7116590a60b097510dc75e874b2fb
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-10-11 15:26:23 +0700

    Complete coffee shop as assignment 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/week07/dialog_demo.dart';
+import 'package:myapp/week07/hw/coffee_shop.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: DialogDemo()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: CoffeeShop()));
 }
