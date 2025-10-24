dab95fd67e02d7bd8bd0eeab9a59029120694630
Sippakornnn <6631501171@lamduan.mfu.ac.th>
2025-10-24 09:24:44 +0700

    Complete drawer demo

--- lib/week09/drawer_demo2.dart
+++ lib/week09/drawer_demo2.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/week09/drawer_widget.dart';
 
-class DrawerDemo extends StatelessWidget {
-  const DrawerDemo({super.key});
+class DrawerDemo2 extends StatelessWidget {
+  const DrawerDemo2({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Drawer Demo')),
       drawer: DrawerWidget(),
-      body: Align(alignment: Alignment.topCenter, child: Text('First page')),
+      body: Align(alignment: Alignment.topCenter, child: Text('Second page')),
     );
   }
 }
